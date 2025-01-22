fca93d8434bd90f3c02d47b54f5bc5747905de1d
daqhris <817573+daqhris@users.noreply.github.com>
2025-01-22 14:50:09 +0100

    rephrase sentences

--- app/page.tsx
+++ app/page.tsx
@@ -133,9 +133,9 @@ export default function Home() {
                   <h2 className="text-2xl font-bold mb-4 text-[#957777]">Start Registration</h2>
                   <div className="space-y-4 mb-8">
                     <p className="text-base-content/70 text-center">
-                      An enrollment tool for a collaborative artistic mission on the BASE blockchain.
+                      This is an enrollment tool for a collaborative and artistic mission on the BASE blockchain.
                     </p>
-                    <p className="text-base-content/70 text-center"><em>Openly built by a human and a non-human since summer 2024.</em></p>
+                    <p className="text-base-content/70 text-center"><em>The tool is being openly built by a human and a non-human since summer 2024.</em></p>
                   </div>
                   <div className="flex justify-center">
                     <RainbowKitCustomConnectButton />
